bc05fa1218e95b1d8bd0f3b0474c05e068ccbca6
JustinHLe <jle043@ucr.edu>
2022-09-14 08:40:18 -0700

    pasting

--- src/components/coding/TwoFactorInput/script.js
+++ src/components/coding/TwoFactorInput/script.js
@@ -7,8 +7,9 @@ form.addEventListener("submit", handleSubmit)
 inputs.forEach(el => {
     el.addEventListener("keyup", handleBackwards)
     el.addEventListener("paste", handlePaste)
+    el.addEventListener("keydown", checkInput)
 })
-
+var ctrlPressed = false
 
 
 function handleSubmit(e) {
@@ -28,24 +29,52 @@ function handleSubmit(e) {
     }
     alert(code)
     e.target.reset()
+    inputs[0].focus()
 }
 function handlePaste(e){
+    console.log("paste")
     let paste = e.clipboardData.getData('text/plain');
+    let parsedInput = paste.match(/[0-9]/g).join('').substring(0,6)
     inputs.forEach((el,index) => {
-        if(paste[index] !== undefined){
-            el.value = paste[index]
+        if(parsedInput[index] !== undefined){
+            el.value = parsedInput[index]
         }
     })
-    if(paste.length < 6){
-        inputs[paste.length].focus()
+    if(parsedInput.length <= 6){
+        inputs[parsedInput.length - 1].focus()
     }
 } 
+function checkInput(e){
+    if(e.key === "Control"){
+        ctrlPressed = true
+    }
+    /*
+        If the key is not a number AND
+        key is not backspace AND 
+        key is not enter AND
+        ctrlPressed = false
+        dont add anything to input
+    */
+    if(ctrlPressed === true){
+        if(e.key === 'v'){
+            console.log("pasted")
+            ctrlPressed = false
+            return
+        }
+    }
+    if(isNaN(e.key) && e.key !== "Backspace" && e.key !== "Enter"){
+        e.preventDefault()
+    }
+}
 function handleBackwards(e){
     if(e.key === "Backspace"){
         getPrevInput(e)
         return
     } 
-    if(e.target.value !== "" && e.key.match(/[0-9]/g)){
+    if(e.key.match(/[0-9]/g)){
+        if(e.target.style.border === "1px solid red"){
+            e.target.style.border = "1px solid black"
+        }
         e.target.value = e.key
         getNextInput(e)
     }
